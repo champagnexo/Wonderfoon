a5545b842a41b9fecb494d7791ea06082d539c1a
Cristian <cristian.mihalcea@icloud.com>
2025-11-09 11:53:46 +0100

    Add files via upload

--- Telefoon1.ino
+++ Telefoon1.ino
@@ -1,25 +1,14 @@
 /***************************************************
-TELEFOON I - BEIGE - CODERDOJO TIENEN
+TELEFOON II - ZWART - CODERDOJO TIENEN
 ****************************************************/
 
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-/**
-Draaischijf
-*/
 #define ROOD 2
 #define BLAUW 3
-
-/**
-Hoorn -> 5V naar (1) op telefoon, uitlezen op (5)
-HIGH = hoorn van de haak, LOW = hoorn op de haak
-*/
-#define ORANJE 5
-
 #define VOLUME 30
-#define COUNTER_MODIFYER 1
 
 long pin2Counter = 0;
 long pin3Counter = 0;
@@ -27,10 +16,12 @@ long idleCounter = 0;
 long antiDubbel = 500;
 boolean pin2LastState;
 boolean pin3LastState;
-boolean pin5LastState;
+bool isPlaying = false;
 int pin2Value = 0;
 int pin3Value = 0;
-int pin5Value = 0;
+const int hoornPin = 4;
+int hoornStaat = 0;
+int lastHoornStaat = 0;
 
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
@@ -38,6 +29,7 @@ void printDetail(uint8_t type, int value);
 
 void setup()
 {
+
   mySoftwareSerial.begin(9600);
   Serial.begin(9600);
   
@@ -54,32 +46,49 @@ void setup()
   }
   Serial.println(F("DFPlayer Mini online."));
   
-  myDFPlayer.volume(30);  //Set volume value. From 0 to 30
-  // myDFPlayer.play(1);  //Play the first mp3  
-  
+  myDFPlayer.volume(15);  //Set volume value. From 0 to 30
+  //  myDFPlayer.play(1);  //Play the first mp3  
   // Initialiseer draaischijf
+  
+  pinMode(hoornPin, INPUT_PULLUP);
   pinMode(ROOD, INPUT_PULLUP);
   pinMode(BLAUW, INPUT_PULLUP);
   pin2LastState = digitalRead(ROOD);
   pin3LastState = digitalRead(BLAUW);
-
-  // initialiseer hoorn
-  pinMode(ORANJE, INPUT_PULLUP);
-  pin5LastState = digitalRead(ORANJE);
-  
   idleCounter = 0;
   pin2Counter = 0;
   pin3Counter = 0;
   pin2Value = 0;
   pin3Value = 0;
-  pin5Value = 0;
+
 }
 
 void loop()
-{  
+{
+
+   hoornStaat = digitalRead(hoornPin);
+   if (hoornStaat != lastHoornStaat) {
+    if (hoornStaat == LOW) {
+      Serial.println("Hoorn van de haak");
+      myDFPlayer.loop(1); // Play track 1
+      myDFPlayer.enableLoop();
+    } else {
+      myDFPlayer.stop();
+      Serial.println("Hoorn op de haak");
+    }
+    lastHoornStaat = hoornStaat;
+  }
+
+
+  static unsigned long timer = millis();
+  
+  if (millis() - timer > 3000) {
+    timer = millis();
+   //myDFPlayer.stop();  //Play next mp3 every 3 seconds
+  }
+  
   boolean pin2State = digitalRead(ROOD);
   boolean pin3State = digitalRead(BLAUW);
-  boolean pin5State = digitalRead(ORANJE);
   
   idleCounter++;
   if (idleCounter==0x30000)
@@ -118,9 +127,9 @@ void loop()
       if (pin3State) {
         Serial.println("io3 0 naar 1");
         Serial.println("Gedraaid nummer: ");
-        Serial.println((pin2Value + 1) % 10);
+        Serial.println((pin2Value) % 10);
         myDFPlayer.stop();
-        myDFPlayer.playFolder(1,(pin2Value + 1) % 10);
+        myDFPlayer.playMp3Folder((pin2Value));
         pin2Value = 0;
       }
       else {
@@ -133,23 +142,8 @@ void loop()
     idleCounter = 0;
   }
 
-  if (pin5LastState != pin5State)
-  {
-  	if (pin3State) {
-    	Serial.println("io5 0 naar 1");
-    	Serial.println("Hoorn op de haak");
-    	pin5Value = 1;
-  	}
-  	else {
-    	Serial.println("io5 1 naar 0");
-      Serial.println("Hoorn van de haak");
-  	}
-    pin5LastState = pin5State;
-    idleCounter = 0;
-  }
-  
   if (myDFPlayer.available()) {
-    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
   }
 }
 
@@ -157,7 +151,7 @@ void loop()
 void printDetail(uint8_t type, int value){
   switch (type) {
     case TimeOut:
-      Serial.println(F(""));
+      Serial.println(F("Time Out!"));
       break;
     case WrongStack:
       Serial.println(F("Stack Wrong!"));
@@ -213,4 +207,5 @@ void printDetail(uint8_t type, int value){
     default:
       break;
   }
+  
 }
