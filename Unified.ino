927a5f69f683db6ca2ded8645d4170e5e9d7971f
Cristian <cristian.mihalcea@icloud.com>
2025-12-14 21:34:38 +0100

    Creation of unified code

---
+++ Unified.ino
@@ -0,0 +1,278 @@
+/***************************************************
+UNIFIED TELEFOON CODE - STAD TIENEN
+****************************************************/
+
+// ============================================
+// CONFIGURATIE: Wijzig deze waarde voor elke telefoon
+// 1 = BEIGE telefoon
+// 2 = ZWART telefoon  
+// 3 = MATTE ZWART telefoon
+// ============================================
+#define TELEFOON_NUMMER 1
+// ============================================
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+/**
+Draaischijf
+*/
+#define ROOD 2
+#define BLAUW 3
+
+/**
+Hoorn pins
+*/
+#if TELEFOON_NUMMER == 1
+  #define HOORN_PIN 5  // ORANJE pin voor telefoon 1
+#else
+  #define HOORN_PIN 4  // Pin 4 voor telefoons 2 en 3
+#endif
+
+#define VOLUME 30
+#define COUNTER_MODIFYER 1
+
+long pin2Counter = 0;
+long pin3Counter = 0;
+long idleCounter = 0;
+long antiDubbel = 500;
+boolean pin2LastState;
+boolean pin3LastState;
+boolean hoornLastState;
+bool isPlaying = false;
+int pin2Value = 0;
+int pin3Value = 0;
+int hoornStaat = 0;
+int lastHoornStaat = 0;
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+void printDetail(uint8_t type, int value);
+
+void setup()
+{
+  mySoftwareSerial.begin(9600);
+  Serial.begin(9600);
+  
+  Serial.println();
+  Serial.print(F("Initializing TELEFOON "));
+  Serial.println(TELEFOON_NUMMER);
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true){
+      delay(0); // Code to compatible with ESP8266 watch dog.
+    }
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  
+  // Volume instelling afhankelijk van telefoon
+  #if TELEFOON_NUMMER == 1
+    myDFPlayer.volume(30);  // Telefoon 1: volume 30
+  #else
+    myDFPlayer.volume(15);  // Telefoons 2 en 3: volume 15
+  #endif
+  
+  // Initialiseer draaischijf
+  pinMode(ROOD, INPUT_PULLUP);
+  pinMode(BLAUW, INPUT_PULLUP);
+  pin2LastState = digitalRead(ROOD);
+  pin3LastState = digitalRead(BLAUW);
+
+  // Initialiseer hoorn
+  pinMode(HOORN_PIN, INPUT_PULLUP);
+  hoornLastState = digitalRead(HOORN_PIN);
+  
+  idleCounter = 0;
+  pin2Counter = 0;
+  pin3Counter = 0;
+  pin2Value = 0;
+  pin3Value = 0;
+}
+
+void loop()
+{  
+  boolean pin2State = digitalRead(ROOD);
+  boolean pin3State = digitalRead(BLAUW);
+  boolean hoornState = digitalRead(HOORN_PIN);
+  
+  // Hoorn detectie - verschilt per telefoon
+  #if TELEFOON_NUMMER == 1
+    // Telefoon 1: logica met pin5State
+    if (hoornLastState != hoornState)
+    {
+      if (pin3State) {
+        Serial.println("Hoorn status gewijzigd");
+        if (hoornState == HIGH) {
+          Serial.println("Hoorn op de haak");
+        } else {
+          Serial.println("Hoorn van de haak");
+        }
+      }
+      hoornLastState = hoornState;
+      idleCounter = 0;
+    }
+  #else
+    // Telefoons 2 en 3: logica met hoornPin
+    if (hoornState != lastHoornStaat) {
+      if (hoornState == LOW) {
+        Serial.println("Hoorn van de haak");
+        myDFPlayer.loop(1); // Play track 1
+        myDFPlayer.enableLoop();
+      } else {
+        myDFPlayer.stop();
+        Serial.println("Hoorn op de haak");
+      }
+      lastHoornStaat = hoornState;
+    }
+  #endif
+
+  // Timer voor telefoons 2 en 3
+  #if TELEFOON_NUMMER != 1
+    static unsigned long timer = millis();
+    if (millis() - timer > 3000) {
+      timer = millis();
+      //myDFPlayer.stop();  //Play next mp3 every 3 seconds
+    }
+  #endif
+  
+  // Idle counter
+  idleCounter++;
+  if (idleCounter==0x30000)
+  {
+    idleCounter = 0;
+    Serial.println("");
+    pin2Value = 0;
+    pin3Value = 0;
+  }
+
+  // Pin 2 (ROOD) detectie
+  if (pin2LastState != pin2State)
+  {
+    if (pin2Counter < antiDubbel)
+      pin2Counter++;
+    else {
+      if (pin2State) {
+        Serial.println("io2 0 naar 1");
+      }
+      else {
+        if (!pin3State)
+          pin2Value++;
+        Serial.println("io2 1 naar 0");
+        Serial.println(pin2Value);
+      }
+      pin2LastState = pin2State;
+      pin2Counter = 0;
+    }
+    idleCounter = 0;
+  }
+
+  // Pin 3 (BLAUW) detectie
+  if (pin3LastState != pin3State)
+  {
+    if (pin3Counter < antiDubbel)
+      pin3Counter++;
+    else {
+      if (pin3State) {
+        Serial.println("io3 0 naar 1");
+        Serial.println("Gedraaid nummer: ");
+        
+        // Nummer berekening en afspelen - verschilt per telefoon
+        #if TELEFOON_NUMMER == 1
+          int nummer = (pin2Value + 1) % 10;
+          Serial.println(nummer);
+          myDFPlayer.stop();
+          myDFPlayer.playFolder(1, nummer);
+        #else
+          int nummer = (pin2Value) % 10;
+          Serial.println(nummer);
+          myDFPlayer.stop();
+          myDFPlayer.playMp3Folder(nummer);
+        #endif
+        
+        pin2Value = 0;
+      }
+      else {
+        Serial.println("io3 1 naar 0");
+      }
+      pin3LastState = pin3State;
+      idleCounter = 0;
+      pin3Counter = 0;
+    }
+    idleCounter = 0;
+  }
+  
+  if (myDFPlayer.available()) {
+    printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
+  }
+}
+
+
+void printDetail(uint8_t type, int value){
+  switch (type) {
+    case TimeOut:
+      #if TELEFOON_NUMMER == 1
+        Serial.println(F(""));
+      #else
+        Serial.println(F("Time Out!"));
+      #endif
+      break;
+    case WrongStack:
+      Serial.println(F("Stack Wrong!"));
+      break;
+    case DFPlayerCardInserted:
+      Serial.println(F("Card Inserted!"));
+      break;
+    case DFPlayerCardRemoved:
+      Serial.println(F("Card Removed!"));
+      break;
+    case DFPlayerCardOnline:
+      Serial.println(F("Card Online!"));
+      break;
+    case DFPlayerUSBInserted:
+      Serial.println("USB Inserted!");
+      break;
+    case DFPlayerUSBRemoved:
+      Serial.println("USB Removed!");
+      break;
+    case DFPlayerPlayFinished:
+      Serial.print(F("Number:"));
+      Serial.print(value);
+      Serial.println(F(" Play Finished!"));
+      break;
+    case DFPlayerError:
+      Serial.print(F("DFPlayerError:"));
+      switch (value) {
+        case Busy:
+          Serial.println(F("Card not found"));
+          break;
+        case Sleeping:
+          Serial.println(F("Sleeping"));
+          break;
+        case SerialWrongStack:
+          Serial.println(F("Get Wrong Stack"));
+          break;
+        case CheckSumNotMatch:
+          Serial.println(F("Check Sum Not Match"));
+          break;
+        case FileIndexOut:
+          Serial.println(F("File Index Out of Bound"));
+          break;
+        case FileMismatch:
+          Serial.println(F("Cannot Find File"));
+          break;
+        case Advertise:
+          Serial.println(F("In Advertise"));
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
